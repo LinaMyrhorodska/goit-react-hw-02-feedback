9c1bab8c4298f71a9218416763f124f9ca039d3b
LinaMyrhorodska <lmyrhorodska2511@gmail.com>
2023-04-24 23:10:07 +0300

    hw-2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,63 @@
-export const App = () => {
+import { Component } from 'react';
+import { GobalStyle } from "../GlobalStyle";
+import { Layout } from "./Layout/Layout";
+import { Section } from './Section/Section';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+import { Notification } from './Notification/Notification';
+
+export class App extends Component {
+state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+};
+  
+  updateState = nameFeedback => {
+    this.setState(oldData => {
+      let object = { ...oldData };
+      object[nameFeedback] = oldData[nameFeedback] + 1;
+      return object; 
+    });
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return Math.floor(
+      (this.state.good /
+        (this.state.good + this.state.neutral + this.state.bad)) *
+        100 || 0 
+    );
+  };
+  
+render () {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React Homework Template
-    </div>
-  );
+    <Layout>
+      <Section
+        title="Please leave feedback">
+      </Section>
+      <FeedbackOptions
+        options={Object.keys(this.state)}
+        onLeaveFeedback={this.updateState}
+      />
+      <Section title="Statistics">
+        
+        {this.countTotalFeedback() === 0 ? (
+          <Notification message="There is no feedback yet..."/>
+        ) : 
+          (<Statistics
+        options={Object.keys(this.state)}
+        statistic={this.state}
+        total={this.countTotalFeedback()}
+        positives={this.countPositiveFeedbackPercentage}
+          />
+          )}
+        </Section>
+       <GobalStyle/>
+    </Layout>
+)
+}
 };
---
+++ src/components/Section/Section.styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Title = styled.h2`
+ font-size: 24px;
+ margin-bottom: 24px;
+`;
+
+
+
+
